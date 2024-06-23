6a6a0dd69b9ffd9e89cb2c46fab06c200acda4b4
Prisha0909 <97585900+Prisha0909@users.noreply.github.com>
2024-06-23 22:34:42 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -7,58 +7,35 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  selectedFile: File;
-  documentType: string;
-  extractedText: string;
-  clauses: any;
+  selectedFile: File | null = null;
+  documentType: string = '';
+  extractedText: string = '';
+  clauses: { section: string, clause: string, sub_section: string | null } | null = null;
 
   constructor(private http: HttpClient) {}
 
-  onFileChanged(event) {
+  onFileChanged(event: any) {
     this.selectedFile = event.target.files[0];
   }
 
   onUpload() {
-    const uploadData = new FormData();
-    uploadData.append('file', this.selectedFile, this.selectedFile.name);
+    if (this.selectedFile) {
+      const uploadData = new FormData();
+      uploadData.append('file', this.selectedFile, this.selectedFile.name);
 
-    this.http.post<any>('http://localhost:5000/upload-pdf', uploadData)
-      .subscribe(
-        (response) => {
-          this.documentType = response.doc_type;
-          this.extractedText = response.text;
-          this.clauses = response.clauses;
-        },
-        (error) => {
-          console.error('Error uploading file', error);
-        }
-      );
+      this.http.post<any>('http://localhost:5000/upload-pdf', uploadData)
+        .subscribe(
+          (response) => {
+            this.documentType = response.doc_type;
+            this.extractedText = response.text;
+            this.clauses = response.clauses;
+          },
+          (error) => {
+            console.error('Error uploading file', error);
+          }
+        );
+    } else {
+      console.error('No file selected');
+    }
   }
 }
-----------
-  <div class="container">
-  <div class="row">
-    <div class="col">
-      <input type="file" (change)="onFileChanged($event)">
-      <button (click)="onUpload()">Upload</button>
-    </div>
-    <div class="col">
-      <p>Document Type: {{ documentType }}</p>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col">
-      <h3>Extracted Text:</h3>
-      <div>{{ extractedText }}</div>
-    </div>
-    <div class="col">
-      <h3>Predicted Clauses:</h3>
-      <div *ngIf="clauses">
-        <p>Section: {{ clauses.section }}</p>
-        <p>Clause: {{ clauses.clause }}</p>
-        <p *ngIf="clauses.sub_section">Sub-section: {{ clauses.sub_section }}</p>
-      </div>
-    </div>
-  </div>
-</div>
