0fdd9126039ffba79d14acf0b04b384833ebc901
Prisha0909 <97585900+Prisha0909@users.noreply.github.com>
2024-06-23 17:59:32 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,41 +1,71 @@
-<div class="extracted-text">
-  <div *ngFor="let paragraph of paragraphs" class="paragraph">
-    <p>{{ paragraph.text }}</p>
-    <p><strong>Predicted Clause:</strong> {{ paragraph.clause }}</p>
-  </div>
-</div>
-------------
-    import { Component, OnInit } from '@angular/core';
-import { DocumentService } from '../document.service';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
 
-@Component({
-  selector: 'app-extracted-text',
-  templateUrl: './extracted-text.component.html',
-  styleUrls: ['./extracted-text.component.css']
+@Injectable({
+  providedIn: 'root'
 })
-export class ExtractedTextComponent implements OnInit {
-  paragraphs: any[] = [];
+export class DocumentService {
+  private documentData = new BehaviorSubject<any>(null);
 
-  constructor(private documentService: DocumentService) { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-    this.documentService.getDocumentData().subscribe(data => {
-      if (data) {
-        this.paragraphs = data.clauses;
-      }
-    });
+  uploadPdf(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<any>('http://localhost:5000/upload-pdf', formData);
+  }
+
+  setDocumentData(data: any): void {
+    this.documentData.next(data);
+  }
+
+  getDocumentData(): Observable<any> {
+    return this.documentData.asObservable();
   }
 }
+----
+  <div class="app">
+  <div class="top-section">
+    <app-pdf-uploader></app-pdf-uploader>
+    <app-doc-type-display></app-doc-type-display>
+  </div>
+  <app-extracted-text></app-extracted-text>
+</div>
+-
+  import { Component } from '@angular/core';
 
-------------
-  .extracted-text {
-  clear: both;
-  margin-top: 20px;
-  padding-left: 20px;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
 }
+-----
+  Module Class (app.module.ts):
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-.paragraph {
-  margin-bottom: 20px;
-}
------------
-  
+import { AppComponent } from './app.component';
+import { PdfUploaderComponent } from './pdf-uploader/pdf-uploader.component';
+import { DocTypeDisplayComponent } from './doc-type-display/doc-type-display.component';
+import { ExtractedTextComponent } from './extracted-text/extracted-text.component';
+import { DocumentService } from './document.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PdfUploaderComponent,
+    DocTypeDisplayComponent,
+    ExtractedTextComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [DocumentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
